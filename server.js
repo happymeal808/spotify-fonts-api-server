3218ea5ab218455b669270579e69525d0eed03bc
gillian downey <gillianldowney12@gmail.com>
2025-04-07 07:33:33 -0700

    Convert to ES module syntax for Railway

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // server.js
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const fetch = require('node-fetch');
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import fetch from 'node-fetch';
 
 dotenv.config();
 
